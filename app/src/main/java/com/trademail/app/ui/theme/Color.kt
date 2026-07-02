26578b5ded15c6e2d88cdf220a6356bcb9b59916
zhco <luycheung@qq.com>
2026-07-02 12:39:27 +0800

    Add Color.kt

---
+++ app/src/main/java/com/trademail/app/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.trademail.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue600 = Color(0xFF1A73E8)
+val Blue700 = Color(0xFF1557B0)
+val Blue50 = Color(0xFFE8F0FE)
+val Gray50 = Color(0xFFF8F9FA)
+val Gray100 = Color(0xFFF1F3F4)
+val Gray200 = Color(0xFFE8EAED)
+val Gray600 = Color(0xFF5F6368)
+val Gray900 = Color(0xFF202124)
+val Green500 = Color(0xFF34A853)
+val Orange500 = Color(0xFFFA9C16)
