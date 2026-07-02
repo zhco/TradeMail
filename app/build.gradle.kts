b12ec9aeb8ebaa1cdb48a6093ddd11b79b2288b4
zhco <luycheung@qq.com>
2026-07-03 06:27:32 +0800

    Fix Room: annotationProcessor -> kapt for Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -93,5 +94,5 @@ dependencies {
     // Room (翻译缓存)
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 }
